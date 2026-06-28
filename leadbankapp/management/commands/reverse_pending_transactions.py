fb565a248aeeda763902933ddf698506d22d9b14
Web Frame <webframe8@gmail.com>
2026-06-28 07:54:10 +0100

    add revers transaction to cron job

--- leadbankapp/management/commands/reverse_pending_transactions.py
+++ leadbankapp/management/commands/reverse_pending_transactions.py
@@ -1,7 +1,25 @@
 from django.core.management.base import BaseCommand
+from leadbankapp.models import AccountTransaction
+from datetime import timedelta
+from django.utils import timezone
+from leadbankapp.services import AccountSevice
 
 class Command(BaseCommand):
     help = "Reverse pending transactions older than 24 hours"
 
     def handle(self, *args, **options):
         print("Cron is working!")
+
+        cutoff = timezone.now() - timedelta(hours=24)
+
+        transactions = AccountTransaction.objects.filter(status="pending", created_at__lte=cutoff)
+
+        for transaction in transactions:
+            AccountSevice.reverse_transaction(transaction)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Checked {transactions.count()} pending transaction(s)."
+            )
+        )
+        
